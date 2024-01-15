282f7c69538e1f5e8ae80e7ab54172e196f1c649
AntonVak <antonv.snab@gmail.com>
2024-01-15 23:54:14 +0200

    Values scroll

--- src/components/OurValues.jsx
+++ src/components/OurValues.jsx
@@ -1,24 +1,38 @@
+import { Container } from "@mui/material";
 import style from "./ourValues.module.css";
 import FlagIcon from "./ui/icons/FlagIcon";
 
 const OurValues = () => {
-    return ( <section className={style.section}>
-        <p className={style.title}>наша сила у наших цінностях</p>
-        <div className={style.values}>
+  const styles = {
+    containerLg: {
+      "&.MuiContainer-maxWidthLg": {
+        width: "100% ",
+      },
+    },
+  };
+  return (
+    <section className={style.section}>
+      <Container sx={{ ...styles.containerLg }}></Container>
+      <p className={style.title}>наша сила у наших цінностях</p>
+      <div className={style.values}>
         <FlagIcon />
-        <div className={style.values_title}>Єднісь</div>
-        <FlagIcon/>
-        <div className={style.values_title}>Взаємодія</div>
-        <FlagIcon/>
-        <div className={style.values_title}>Повага</div>
-        <FlagIcon/>
-        <p className={style.values_title}>Віра в перемогу <br></br>
-України</p>
-        <FlagIcon/>
-        <div className={style.values_title}>Прозорість і<br></br>звітність</div>
-            
-        </div>
-    </section> );
-}
- 
+        <p className={style.values_title}>Єднісь</p>
+        <FlagIcon />
+        <p className={style.values_title}>Взаємодія</p>
+        <FlagIcon />
+        <p className={style.values_title}>Повага</p>
+        <FlagIcon />
+        <p className={style.values_title}>
+          Віра в перемогу <br></br>
+          України
+        </p>
+        <FlagIcon />
+        <p className={style.values_title}>
+          Прозорість і<br></br>звітність
+        </p>
+      </div>
+    </section>
+  );
+};
+
 export default OurValues;

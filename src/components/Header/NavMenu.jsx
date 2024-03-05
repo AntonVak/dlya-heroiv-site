2a9ded976c06e4b184b07176f3e97e9e63512b22
AntonVak <antonv.snab@gmail.com>
2024-03-05 12:43:07 +0200

    head

--- src/components/Header/NavMenu.jsx
+++ src/components/Header/NavMenu.jsx
@@ -17,7 +17,9 @@ const NavMenu = ({ isNavActive, handleNavClick }) => {
   return (
     <nav className={styles.nav_menu}>
       <div className={styles.container}>
-        <div className={styles.header_row}>
+        <div className={`${styles.header_row} ${
+              isNavActive ? styles.mobile_header_row : ""
+            }`} >
           <div className={styles.header_logo}>
             <a href="#">
               <img src={LogoIcon} alt="Logo" />
@@ -86,6 +88,72 @@ const NavMenu = ({ isNavActive, handleNavClick }) => {
       <div
         className={`${styles.mobile_menu} ${isNavActive ? styles.active2 : ""}`}
       >
+       {/* <div className={styles.header_row}>
+          <div className={styles.header_logo}>
+            <a href="#">
+              <img src={LogoIcon} alt="Logo" />
+            </a>
+          </div>
+          <div
+            className={`${styles.header_menu} ${
+              isNavActive ? styles.activ : ""
+            }`}
+          >
+            <div className={`${styles.header_nav} `}>
+              <a href="#">
+                <div className={styles.header_nav_text}>Про фонд</div>
+              </a>
+              <a href="#">
+                <div className={styles.header_nav_text}>Проєкти</div>
+              </a>
+              <a href="*">
+                <div className={styles.header_nav_text}>Звітність</div>
+              </a>
+              <a href="*">
+                <div className={styles.header_nav_text}>Стати партнером</div>
+              </a>
+            </div>
+            <div className={styles.vertical_line}></div>
+            <div className={styles.header_support}>
+              <HeaderFlagCicle />
+              <a href="#">
+                <div
+                  className={`${styles.header_nav_text} ${styles.color_text}`}
+                >
+                  Підтримати
+                </div>
+              </a>
+            </div>
+
+            <div className={styles.vertical_line}></div>
+            <div className={styles.header_social}>
+              <a href="#" className={styles.icon_social}>
+                <TwitterIcon />
+              </a>
+              <a href="#">
+                <img src={FacebookIcon} alt="" />
+              </a>
+              <a href="#">
+                <img src={InstagramIcon} alt="" />
+              </a>
+              <a href="#">
+                <img src={YoutubeIcon} alt="" />
+              </a>
+              <a href="#">
+                <img src={TiktokIcon} alt="" />
+              </a>
+            </div>
+            <div className={styles.vertical_line}></div>
+            <div className={styles.header_web}>
+              <img src={web} alt="" />
+            </div>
+            <div className={styles.burger_wrapper}>
+             
+            </div>
+          </div>
+        </div> */}
+       
+
         <div className={`${styles.header_nav2} `}>
           <a href="#">
             <div className={styles.header_nav_text}>Про фонд</div>
@@ -103,23 +171,7 @@ const NavMenu = ({ isNavActive, handleNavClick }) => {
         <div className={`${styles.btn_active} `}>
           <ButtonToPartner name="Підтримати" />
         </div>
-        <div className={styles.social_burger}>
-          <a href="#" className={styles.icon_social}>
-            <TwitterIcon />
-          </a>
-          <a href="#">
-            <img src={FacebookIcon} alt="" />
-          </a>
-          <a href="#">
-            <img src={InstagramIcon} alt="" />
-          </a>
-          <a href="#">
-            <img src={YoutubeIcon} alt="" />
-          </a>
-          <a href="#">
-            <img src={TiktokIcon} alt="" />
-          </a>
-        </div>
+       
         <div className={styles.language_burger}>
           <a href="">UA</a>
           <a href="">ENG</a>
